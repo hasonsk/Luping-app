eda63d495d31c13000d1de2c402abd93641c21dc
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2025-02-22 20:33:03 +0700

    add loki, promptail, grafana for log management

--- luping.server/src/configs/logger.js
+++ luping.server/src/configs/logger.js
@@ -1,15 +1,41 @@
 import { createLogger, transports, format } from "winston";
+import fs from "fs";
+
+// Ensure logs folder exists
+const logDir = "./logs";
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const logger = createLogger({
   level: "info",
   format: format.combine(
-    format.colorize(),
-    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    format.printf(
-      (info) => `[${info.timestamp}] ${info.level}: ${info.message}`,
-    ),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }) // Add timestamp
   ),
-  transports: [new transports.Console()],
+  transports: [
+    // Console Log (Formatted)
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          (info) => `[${info.timestamp}] ${info.level}: ${info.message}`
+        )
+      ),
+    }),
+
+    // Combined Log File (JSON Format)
+    new transports.File({
+      filename: "logs/combined.log",
+      format: format.combine(format.json()),
+    }),
+
+    // Error Log File (JSON Format)
+    new transports.File({
+      filename: "logs/errors.log",
+      level: "error",
+      format: format.combine(format.json()),
+    }),
+  ],
 });
 
 export default logger;
